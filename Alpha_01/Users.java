0acd70e4330a23d5df7f31c9fd35e0133e6e644d
ga2006235919 <eric20061220@gmail.com>
2015-10-15 18:55:57 +0800

    Update Users.java

--- Alpha_01/Users.java
+++ Alpha_01/Users.java
@@ -1,27 +1,43 @@
+/********************************************
+* Project Team:	
+* Students: 
+* Couse: OOP 2015 Sem 1, HDIT
+*
+* Users.java: 
+********************************************/
+
 class Users{
+	
+	//data members
 	public int userID;
-	public String userName;
 	public String userType;
+	private String userName;
 	private String password;
 	protected String email;
 	protected String phone;
 	public static int totalNumOfUser = 0;
-
+	
+	//constructors
 	Users(){
 
 	}
-
+	
 	Users(String userName, String password, String userType){
 		this.userName = userName;
 		//encrypt password
 		//CryptWithMD5 CMD5 = new CryptWithMD5();
 		this.password = CryptWithMD5.cryptWithMD5(password);
 		this.userType = userType;
+		
+		this.userID = totalNumOfUser;
+		this.totalNumOfUser++;
 	}
 
+	//methods
 	public boolean validateUser(String inName, String inPassword){
 		return (userName.equals(inName) && password.equals(inPassword));
 	}
+	
 }
 
 /*public static void main(String[] args) {
