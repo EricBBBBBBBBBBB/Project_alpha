96b092242833a2138ed93ef3da46da5f86dbe36c
ga2006235919 <eric20061220@gmail.com>
2015-10-15 15:48:37 +0800

    Create TrainingRecordSystem.java

---
+++ TrainingRecordSystem.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class TrainingRecordSystem {
+
+	public static void main(String[] args) {
+
+		String userName;
+		String password;
+		
+		System.out.println("**********************************");
+		System.out.println(" Welcome to Training Record System");
+		System.out.println("**********************************");
+		
+		Scanner scanner = new Scanner(System.in);
+
+		
+		scanner.close();
+	}
+}
