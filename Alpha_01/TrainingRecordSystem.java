fa6dc4f089b1f0e338214d2cea75dcbe3295585b
Chuen509 <ccw@Chuen.local>
2015-10-15 16:32:49 +0800

    bug Fix #-1

--- Alpha_01/TrainingRecordSystem.java
+++ Alpha_01/TrainingRecordSystem.java
@@ -8,6 +8,8 @@ public class TrainingRecordSystem {
 		String userName;
 		String password;
 
+		int loginID = -1;	// no login ID -> empty
+
 		// Load user information - *For test only*
 		Trainee[] trainee = {
 			new Trainee("e1", "e"),
@@ -28,26 +30,25 @@ public class TrainingRecordSystem {
 		System.out.println("**********************************");
 		
 		Scanner scanner = new Scanner(System.in);
-	
-		do{
-			// enter user name
-			System.out.print("Please enter your user name: ");
-			while ((userName = scanner.nextLine()).isEmpty()){
-				System.out.print("User name cannot be empty, Please enter again: ");
-			} 
-			
-			//enter password
-			System.out.print("Please enter your password: ");
-			while ((password = scanner.nextLine()).isEmpty()){
-				System.out.print("Password cannot be empty, Please enter again: ");
-			} 
-			
-			//validate
-			if(trainee.validateUser(userName, password)) System.out.println("\nLogin in successfully!");
-				else System.out.println("\nLogin fail, please login again.");
-				
-		}while(!trainee.validateUser(userName, password));		//repeat if login fail
 
+		System.out.print("Please enter your user name: ");
+		while ((userName = scanner.nextLine()).isEmpty()){
+			System.out.print("User name cannot be empty, Please enter again: ");
+		} 
+		System.out.print("Please enter your password: ");
+		while ((password = scanner.nextLine()).isEmpty()){
+			System.out.print("Password cannot be empty, Please enter again: ");
+		} 
+		for (int i = 0; i < trainee.length; i++) {
+			if(trainee[i].validateUser(userName, password)){
+				System.out.println("\nLogin in successfully!");
+				loginID = i;
+				System.out.println("The ID is " + loginID);
+				break;
+			}
+		}
+		if(loginID == -1)
+			System.out.println("\nLogin fail, please login again.");
 		scanner.close();
 	}
 }
