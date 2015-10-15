3a3f017495bf41457fba4cbf881c6b5a5e90a312
ga2006235919 <eric20061220@gmail.com>
2015-10-15 15:37:43 +0800

    update1

--- Users.java
+++ Users.java
@@ -18,11 +18,76 @@ public class Trainee{
 		
 	}
 	
+	Trainee(String name, String password){
+		
+		//username
+		userName = name;
+		
+		//encrypt password
+		CryptWithMD5 CMD5 = new CryptWithMD5();
+		cryptPassword = CryptWithMD5.cryptWithMD5(password);
+		
+	}
+	
 	//method
-	plubic void browse(){
+	public void browse(){
+		
+	}
+	
+	public void join(int cid){
+		
+	}
+	
+	public void display(){
+		
+	}
+	
+}
+
+public class Trainer{
+	
+	//data member
+	public static int TrainerID ;
+	private String userName;
+	private int phone;
+	private String email;
+		
+	private String acount;
+	private String cryptPassword;
+	
+	//constructor
+	Trainer(){
+		
+	}
+	
+	Trainer(String name){
+		
+	}
+	
+	Trainer(String name, String password){
+		
+		//username
+		userName = name;
+		
+		//encrypt password
+		CryptWithMD5 CMD5 = new CryptWithMD5();
+		cryptPassword = CryptWithMD5.cryptWithMD5(password);
+		
+	}
+	
+	//method
+	public void list(){
+		
+		
+	}
+	
+	public void track(String name){
+		
 		
 	}
 	
 	
-	
 }
+
+
+
