c7ae426c4d5f8c3b94c2939468dd92d6691497bd
ga2006235919 <eric20061220@gmail.com>
2015-10-20 10:54:45 +0800

    Users class
    Traniees class //testing

---
+++ Alpha_02/Users.java
@@ -0,0 +1,65 @@
+/********************************************
+* Project Team:	
+* Students: 
+* Couse: OOP 2015 Sem 1, HDIT
+*
+* Users.java: 
+********************************************/
+
+class Users{
+	
+	//data members
+	public int userID;
+	public String userType;
+	private String userName;
+	private String password;
+	protected String email;
+	protected String phone;
+	public static int totalNumOfUser = 0;
+	
+	//constructors
+	Users(){
+
+	}
+	
+	Users(String userName, String password, String userType){
+		this.userName = userName;
+		//encrypt password
+		//CryptWithMD5 CMD5 = new CryptWithMD5();
+		this.password = CryptWithMD5.cryptWithMD5(password);
+		this.userType = userType;
+		
+		this.userID = totalNumOfUser;
+		this.totalNumOfUser++;
+	}
+
+	//methods
+	public boolean validateUser(String inName, String inPassword){
+		return (userName.equals(inName) && password.equals(inPassword));
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getUserType() {
+		return this.userType;
+	}
+	
+	public int getUserID() {
+		return this.userID;
+	}
+	
+}
+
+class Trainees extends Users{
+	
+	//data members
+	
+	//constructors
+	
+	//methods
+	public void joinCourse(String name){
+		
+	}
+}
