e8c21ad82a7a513e950b101bc7e465ed4dc076f2
ga2006235919 <eric20061220@gmail.com>
2015-10-20 10:54:04 +0800

    System main java

---
+++ Alpha_02/TrainingRecordSystem.java
@@ -0,0 +1,24 @@
+/********************************************
+* Project Team:	
+* Students: 
+* Couse: OOP 2015 Sem 1, HDIT
+*
+* TrainingRecordSystem.java: 
+********************************************/
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TrainingRecordSystem {
+
+	public static void main(String[] args) {
+
+		System.out.println("***********************************");
+		System.out.println(" Welcome to Training Record System");
+		System.out.println("***********************************");
+		
+		Login login = new Login(); //login
+		System.out.println(login.LoginStatus());
+		
+	}
+}
