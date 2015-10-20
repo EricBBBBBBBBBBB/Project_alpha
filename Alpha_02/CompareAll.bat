bf6136564b12dff9eea211c9530259cb243ac124
ga2006235919 <eric20061220@gmail.com>
2015-10-20 13:22:19 +0800

    Compare use

---
+++ Alpha_02/CompareAll.bat
@@ -0,0 +1,8 @@
+javac Users.java
+javac Courses.java
+javac Menus.java
+javac Login.java
+javac CryptWithMD5.java
+javac TrainingRecordSystem.java
+pause
+
---
+++ Alpha_02/Start.bat
@@ -0,0 +1,2 @@
+java TrainingRecordSystem
+pause
