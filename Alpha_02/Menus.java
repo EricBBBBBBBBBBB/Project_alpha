88b4a180d0b94a3de0edf04393562af1f1d22384
ga2006235919 <eric20061220@gmail.com>
2015-10-20 10:56:54 +0800

    Menus class //testing

---
+++ Alpha_02/Menus.java
@@ -0,0 +1,40 @@
+/********************************************
+* Project Team:	
+* Students: 
+* Course: OOP 2015 Sem 1, HDIT
+*
+* Menus.java: 
+********************************************/
+
+class Menus{
+	
+	//data members
+	
+	
+	//constructors
+	
+	
+	
+	//methods
+	
+}
+
+/*General
+	-Change Personal Information
+	-
+*/
+
+/*Trainee
+	-Browse crrent available courses (from Courses.java)
+	-
+*/
+
+/*Trainer
+	-
+	-
+*/
+
+/*Admin
+	-
+	-
+*/
