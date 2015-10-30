4dd0db93f9bee84c8e1a63352ae64cd44b826a2e
ga2006235919 <eric20061220@gmail.com>
2015-10-30 16:36:18 +0800

    Update Menus.java

--- Alpha_03/Menus.java
+++ Alpha_03/Menus.java
@@ -9,29 +9,27 @@ import java.io.*;
 import java.util.*;
 
 class Menus{
+	
+	//data members
 	public char inChar;
 	public Users user;
-	//data members
-	
 	
 	//constructors
 	Menus(Users user) {
 		this.user = user;
 	}
 	
-	
-	
 	//methods
 	public void mainMenu() {
 		switch(user.getUserType()) {
-			case 0:
+			case "Tarinee":
 				traineeMenu();
 				break;
-			case 1:
-				trainerMenu();
+			case "Tariner":
+				traineeMenu();
 				break;
-			case 2:
-				adminMenu();
+			case "Admin":
+				traineeMenu();
 				break;
 			default:
 				System.out.println("Unknown Comment");
@@ -39,31 +37,32 @@ class Menus{
 	}
 
 	public void traineeMenu() {
+		
 		Scanner in = new Scanner(System.in);
 
-		System.out.println("------- Trainee Menu -------");
-		System.out.println("1. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("2. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("3. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("4. xxxxxxxxxxxxxxxxxxxxxx");
+		System.out.println("------- " + user.getUserType() + " Menu -------");
+		System.out.println("1. Self-Register");
+		System.out.println("2. Browse current training courses");
+		System.out.println("3. Join training courses");
+		System.out.println("4. Display training courses");
 		System.out.println("5. General Menu");
 		System.out.println("Enter q for quit.");
-
-
+	
+		
 		inChar = in.next().charAt(0);
 
 		switch(inChar) {
 			case '1':
-				System.out.println("You choose number 1");
+				user.register();
 				break;
 			case '2':
-				System.out.println("You choose number 2");
+				user.browseCourse();
 				break;
 			case '3':
-				System.out.println("You choose number 3");
+				user.joinCourse();
 				break;
 			case '4':
-				System.out.println("You choose number 4");
+				user.displayCourse();
 				break;
 			case '5':
 				System.out.println("You choose number 5");
@@ -76,102 +75,5 @@ class Menus{
 
 		in.close();
 	}
-
-		public void trainerMenu() {
-		Scanner in = new Scanner(System.in);
-
-		System.out.println("------- Trainer Menu -------");
-		System.out.println("1. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("2. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("3. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("4. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("5. General Menu");
-		System.out.println("Enter q for quit.");
-
-
-		inChar = in.next().charAt(0);
-
-		switch(inChar) {
-			case '1':
-				System.out.println("You choose number 1");
-				break;
-			case '2':
-				System.out.println("You choose number 2");
-				break;
-			case '3':
-				System.out.println("You choose number 3");
-				break;
-			case '4':
-				System.out.println("You choose number 4");
-				break;
-			case '5':
-				System.out.println("You choose number 5");
-				break;
-			case 'Q':
-			case 'q':
-				System.out.println("You choose Q ");
-				break;
-		}
-
-		in.close();
-	}
-
-	public void adminMenu() {
-		Scanner in = new Scanner(System.in);
-
-		System.out.println("------- Admin Menu -------");
-		System.out.println("1. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("2. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("3. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("4. xxxxxxxxxxxxxxxxxxxxxx");
-		System.out.println("5. General Menu");
-		System.out.println("Enter q for quit.");
-
-
-		inChar = in.next().charAt(0);
-
-		switch(inChar) {
-			case '1':
-				System.out.println("You choose number 1");
-				break;
-			case '2':
-				System.out.println("You choose number 2");
-				break;
-			case '3':
-				System.out.println("You choose number 3");
-				break;
-			case '4':
-				System.out.println("You choose number 4");
-				break;
-			case '5':
-				System.out.println("You choose number 5");
-				break;
-			case 'Q':
-			case 'q':
-				System.out.println("You choose Q ");
-				break;
-		}
-
-		in.close();
-	}
+	
 }
-
-/*General
-	-Change Personal Information
-	-
-*/
-
-/*Trainee
-	-Browse crrent available courses (from Courses.java)
-	-
-*/
-
-/*Trainer
-	-
-	-
-*/
-
-/*Admin
-	-
-	-
-*/
